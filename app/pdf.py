2857e8a4fb69551de37e73d590d87f0a11a1b009
Man Tran <tranduyminhman@gmail.com>
2022-03-30 00:33:27 +0700

    update logging

--- app/pdf.py
+++ app/pdf.py
@@ -1,3 +1,4 @@
+from asyncio.log import logger
 import csv
 from datetime import datetime
 from slugify import slugify
@@ -5,11 +6,11 @@ from slugify import slugify
 import utils
 from utils import pdf_processor
 
-logger = utils.get_logger()
-
 def build_payslip():
     date_time = utils.input_date_time()
-    with open("Bang_Luong_Mau.csv", "r") as f:
+    fileCSV = input('Nhap ten file:')
+    logger = utils.get_logger(fileCSV, date_time)
+    with open(fileCSV, "r") as f:
         reader = csv.reader(f, delimiter="\t")
         for i, line in enumerate(reader):
             row = line[0].split(';')
@@ -25,8 +26,12 @@ def build_payslip():
             list_name = utils.get_list_words(input_data)
             filename = '__'.join(list_name) + '.pdf'
             list_name.append('encript')
+            filenameEncript = '__'.join(list_name) + '.pdf'
             
             pdf_processor.generate(path, filename, input_data)
+            # pdf_processor.set_password(path, filename, filenameEncript, input_data)
+
+            # send_mail.send_mail(path, filenameEncript, input_data)
 
 if __name__ == "__main__":
     build_payslip();
