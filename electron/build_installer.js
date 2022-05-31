f0f3d3a59a678006a54a2a465fee8e0a0b6afbe7
Man Tran <man.tran@watasolutions.com>
2022-05-31 16:51:41 +0700

    wip

---
+++ electron/build_installer.js
@@ -0,0 +1,34 @@
+// 1. Import Modules
+const { MSICreator } = require('electron-wix-msi');
+const path = require('path');
+
+// 2. Define input and output directory.
+// Important: the directories must be absolute, not relative e.g
+const APP_DIR = path.resolve(__dirname, './out/payslip-app-win32-x64');
+const OUT_DIR = path.resolve(__dirname, './out');
+
+// 3. Instantiate the MSICreator
+const msiCreator = new MSICreator({
+    appDirectory: APP_DIR,
+    outputDirectory: OUT_DIR,
+
+    // Configure metadata
+    description: 'This is an application to serve compressed pdf files and then send the email.',
+    exe: 'payslip-app',
+    name: 'Payslip App',
+    manufacturer: 'Man Tran',
+    version: '0.1.0',
+    arch: 'x64',
+    bundled: true,
+    // Configure installer User Interface
+    ui: {
+        chooseDirectory: true
+    },
+});
+
+// 4. Create a .wxs template file
+msiCreator.create().then(function(){
+
+    // Step 5: Compile the template to a .msi file
+    msiCreator.compile();
+});
--- electron/python/utils/db.py
+++ electron/python/utils/db.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 
 def connect_db(userDataDir):
-    con = sqlite3.connect(userDataDir + '/payslip.db')
+    con = sqlite3.connect(userDataDir + '/databases/Databases.db')
     cur = con.cursor()
     return con, cur
 
