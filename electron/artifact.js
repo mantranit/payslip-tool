ac00e077a5303aa1536b0080093324d3f85a41a8
Man Tran <man.tran@watasolutions.com>
2022-06-02 15:36:46 +0700

    using information from package.json

--- electron/artifact.js
+++ electron/artifact.js
@@ -1,33 +1,33 @@
 // 1. Import Modules
+const pjson = require('./package.json');
 const { MSICreator } = require('electron-wix-msi');
-
 const path = require('path');
 
 // 2. Define input and output directory.
 // Important: the directories must be absolute, not relative e.g
-const APP_DIR = path.resolve(__dirname, './out/payslip-app-win32-x64');
+const APP_DIR = path.resolve(__dirname, `./out/${pjson.name}-win32-x64`);
 const OUT_DIR = path.resolve(__dirname, './out');
 
-// 4. Instantiate the MSICreator
+// 3. Instantiate the MSICreator
 const msiCreator = new MSICreator({
     appDirectory: APP_DIR,
     outputDirectory: OUT_DIR,
 
     // Configure metadata
-    description: 'This is an application to serve compressed pdf files and then send the email.',
-    exe: 'payslip-app',
+    description: pjson.description,
+    exe: pjson.name,
     name: 'Payslip App',
-    manufacturer: 'Man Tran',
-    version: '0.1.0',
+    manufacturer: pjson.author,
+    version: pjson.version,
     arch: 'x64',
-    bundled: true,
+    autoLaunch: true,
     // Configure installer User Interface
     ui: {
         chooseDirectory: true
     },
 });
 
-// 5. Create a .wxs template file
+// 4. Create a .wxs template file
 msiCreator.create().then(function(){
     // Step 5: Compile the template to a .msi file
     msiCreator.compile();
--- electron/copyfile.js
+++ electron/copyfile.js
@@ -1,9 +1,13 @@
+// 1. Import Modules
+const pjson = require('./package.json');
 const path = require('path');
 // include fs-extra package
-var fs = require("fs-extra");
+const fs = require("fs-extra");
 
+// 2. Define input and output directory.
+// Important: the directories must be absolute, not relative e.g
 const PYTHON_DIR = path.resolve(__dirname, './python');
-const PYTHON_DES_DIR = path.resolve(__dirname, './out/payslip-app-win32-x64/python');
+const PYTHON_DES_DIR = path.resolve(__dirname, `./out/${pjson.name}-win32-x64/python`);
 
 // 3. Copy source folder to destination
 fs.copy(PYTHON_DIR, PYTHON_DES_DIR, function (err) {
