bd77709d14850e9538a40785c4582d0e2970bce3
Man Tran <tranduyminhman@gmail.com>
2024-07-15 03:50:06 +0700

    copy

--- electron-forge/forge.config.js
+++ electron-forge/forge.config.js
@@ -1,32 +1,52 @@
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const path = require("path");
+const fs = require("fs-extra");
+const package = require("./package.json");
 
 module.exports = {
+  hooks: {
+    postPackage: async (forgeConfig, packageResult) => {
+      console.log(forgeConfig, packageResult);
+      // const PYTHON_DIR = path.resolve(__dirname, "./python");
+      // const PYTHON_DES_DIR = path.resolve(
+      //   __dirname,
+      //   `./out/${outputPaths}/python`
+      // );
+      // fs.copy(PYTHON_DIR, PYTHON_DES_DIR, function (err) {
+      //   if (err) {
+      //     console.log("An error occured while copying PYTHON folder.");
+      //     return console.error(err);
+      //   }
+      //   console.log("Copy PYTHON completed!");
+      // });
+    },
+  },
   packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
     // Fuses are used to enable/disable various Electron functionality
